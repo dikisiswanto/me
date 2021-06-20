4d8e1b7fa5a4c21c5f600bfc2397c682ee3c8a3b
Diki Siswanto <dickyachmady@gmail.com>
2021-06-20 12:41:24 +0800

    feat(transition): add transition between page moving

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,38 @@
-import { ChakraProvider, Stack } from '@chakra-ui/react';
+import {
+  Box, ChakraProvider, EASINGS, Stack,
+} from '@chakra-ui/react';
+import { AnimatePresence, motion } from 'framer-motion';
 import NextNProgress from 'nextjs-progressbar';
 import Head from 'next/head';
 import NavBar from '../components/navbar';
 import theme from '../theme';
 import meta from '../data/meta.json';
 
-function MyApp({ Component, pageProps }) {
+const MotionBox = motion(Box);
+
+const TRANSITION_SLIDE_FADE_VARIANTS = {
+  initial: {
+    opacity: 0,
+    x: 0,
+    y: -15,
+  },
+  enter: {
+    duration: 0.2,
+    ease: EASINGS.easeOut,
+    opacity: 1,
+    x: 0,
+    y: 0,
+  },
+  exit: {
+    duration: 0.1,
+    ease: EASINGS.easeIn,
+    opacity: 0,
+    x: 0,
+    y: 15,
+  },
+};
+
+function MyApp({ Component, pageProps, router }) {
   return (
     <ChakraProvider theme={theme}>
       <Head>
@@ -24,7 +51,18 @@ function MyApp({ Component, pageProps }) {
           showOnShallow
         />
         <NavBar />
-        <Component {...pageProps} />
+        <AnimatePresence exitBeforeEnter>
+          <MotionBox
+            w="full"
+            key={router.route}
+            animate="enter"
+            exit="exit"
+            initial="initial"
+            variants={TRANSITION_SLIDE_FADE_VARIANTS}
+          >
+            <Component {...pageProps} />
+          </MotionBox>
+        </AnimatePresence>
       </Stack>
     </ChakraProvider>
   );
